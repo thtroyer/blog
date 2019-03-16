b7ae06c4cc670993f314bc57d71c4022079c22f4
Tom Troyer <tom.troyer@gmail.com>
2019-03-15 22:18:23 -0400

    Switching to bootstrap4.  Adding cards to blog home

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,12 +12,24 @@ class BlogController extends AbstractController
     /**
      * @Route("/")
      * @Route("/blog")
+     */
+    public function blogHome($postName = "Some blog title")
+    {
+        return $this->render(
+            'blog.html.twig',
+            [
+                'title' => $postName,
+            ]
+        );
+    }
+
+    /**
      * @Route("/blog/{postName}")
      */
     public function showPost($postName = "default")
     {
         return $this->render(
-            'blog.html.twig',
+            'blogpost.html.twig',
             [
                 'title' => $postName,
             ]
