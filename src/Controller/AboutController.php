ec9ab9871372f65f3ceced0d6957ea4e59942f5b
Tom Troyer <tom.troyer@gmail.com>
2019-03-23 22:54:18 -0400

    Experimenting with entities

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +14,26 @@ class AboutController extends AbstractController
     /**
      * @Route("/about", name="about_page")
      */
-    public function showPost($postName = "default")
+    public function showPost($postName = "default", EntityManagerInterface $entityManager)
     {
+        $userRepository = $entityManager->getRepository(User::class);
+        $users = $userRepository->findAll();
+        $user_data = [];
+        foreach ($users as $user) {
+            $user_data[] = [
+                'id' => $user->getId(),
+                'name' => $user->getNameToShow(),
+                'username' => $user->getUsername(),
+                'dateCreated' => $user->getDateCreated()->format('Y-m-d H:i:s'),
+                'lastModified' => $user->getLastModified()->format('Y-m-d H:i:s'),
+            ];
+        }
+
         return $this->render(
             'about.html.twig',
             [
                 'title' => $postName,
+                'user_data' => print_r($user_data,true),
             ]
         );
     }
